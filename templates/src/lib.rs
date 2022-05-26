e53c2c8e6dfecb95ce280f2a407535d407ff7122
Remo Senekowitsch <contact@remlse.dev>
2022-05-26 09:20:58 +0200

    Add unit test examples

--- templates/src/lib.rs
+++ templates/src/lib.rs
@@ -1,3 +1,17 @@
+#[cfg(test)]
+mod test_add_one {
+    use super::*;
+
+    #[test]
+    fn some_test() {
+        assert_eq!(add_one(1), 2)
+    }
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
 pub fn part1(input: &str) -> i32 {
     todo!()
 }
--- templates/src/main.rs
+++ templates/src/main.rs
@@ -3,5 +3,5 @@ use dayXX::{part1, part2};
 fn main() {
     let input = include_str!("../input/input.txt");
     println!("{}", part1(input));
-    // println!("{}", part2(input));
+    println!("{}", part2(input));
 }
