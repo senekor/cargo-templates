7c9ae97bed29c051ff10e1cb4b9a4f0bc295a8d2
Remo Senekowitsch <contact@remlse.dev>
2022-08-30 11:45:45 +0200

    Improve aoc template

---
+++ aoc/{{year}}/{{day}}/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn part1(input: &str) -> usize {
+    todo!()
+}
+
+pub fn part2(input: &str) -> usize {
+    todo!()
+}
+
+// #[cfg(test)]
+// mod unit_tests {
+//     use super::*;
+
+//     #[test]
+//     fn sanity_check() {
+//         assert_eq!(1 + 1, 2)
+//     }
+// }
