a243c5ec56b0bad7cd4dd98e130a18e9a1a25cf1
Remo Senekowitsch <contact@remlse.dev>
2022-12-10 17:56:01 +0100

    Reorganize integration tests

--- aoc/{{year}}/{{day}}/tests/integration.rs
+++ aoc/{{year}}/{{day}}/tests/integration.rs
@@ -1,26 +1,27 @@
 use {{project-name}}::{part1, part2};
+
+#[allow(unused)]
 use utils::fail::Fail;
+
+static SAMPLE: &str = include_str!("../input/sample.txt");
+static INPUT: &str = include_str!("../input/input.txt");
 
 #[test]
 fn test_part1_sample() {
-    let input = include_str!("../input/sample.txt");
-    assert_eq!(part1(input), Fail);
+    assert_eq!(part1(SAMPLE), Fail);
 }
 
 #[test]
 fn test_part1() {
-    let input = include_str!("../input/input.txt");
-    assert_eq!(part1(input), Fail);
+    assert_eq!(part1(INPUT), Fail);
 }
 
 #[test]
 fn test_part2_sample() {
-    let input = include_str!("../input/sample.txt");
-    assert_eq!(part2(input), Fail);
+    assert_eq!(part2(SAMPLE), Fail);
 }
 
 #[test]
 fn test_part2() {
-    let input = include_str!("../input/input.txt");
-    assert_eq!(part2(input), Fail);
+    assert_eq!(part2(INPUT), Fail);
 }
