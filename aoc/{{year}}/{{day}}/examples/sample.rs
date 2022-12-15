a4cac82b54302f49515911637555286dbb27a823
Remo Senekowitsch <contact@remlse.dev>
2022-12-15 11:01:02 +0100

    Use main template macro from utils

--- aoc/{{year}}/{{day}}/examples/sample.rs
+++
@@ -1,9 +0,0 @@
-use {{project-name}}::{part1, part2};
-use utils::output::Output;
-
-static SAMPLE: &str = include_str!("../input/sample.txt");
-
-fn main() {
-    println!("(sample) part1: {}", part1(SAMPLE).output());
-    println!("(sample) part2: {}", part2(SAMPLE).output());
-}
--- aoc/{{year}}/{{day}}/src/main.rs
+++ aoc/{{year}}/{{day}}/src/main.rs
@@ -1,9 +1,3 @@
-use {{project-name}}::{part1, part2};
-use utils::output::Output;
-
-static INPUT: &str = include_str!("../input/input.txt");
-
 fn main() {
-    println!("part1: {}", part1(INPUT).output());
-    println!("part2: {}", part2(INPUT).output());
+    utils::main!({{project-name}});
 }
