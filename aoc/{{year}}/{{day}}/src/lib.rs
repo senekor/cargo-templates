33fe16727143f23cf9676d0bd7e89442d7f1b0f8
Remo Senekowitsch <contact@remlse.dev>
2023-04-23 20:26:53 +0200

    update manifest, add nom parsing stubs

--- aoc/{{year}}/{{day}}/src/lib.rs
+++ aoc/{{year}}/{{day}}/src/lib.rs
@@ -1,7 +1,11 @@
+mod parse;
+
 pub fn part1(input: &str) -> usize {
+    let (_, numbers) = parse::numbers(input).unwrap();
     0
 }
 
-pub fn part2(_input: &str) -> usize {
+pub fn part2(input: &str) -> usize {
+    let (_, _numbers) = parse::numbers(input).unwrap();
     0
 }
---
+++ aoc/{{year}}/{{day}}/src/parse.rs
@@ -0,0 +1,18 @@
+use utils::{
+    nom::{character::complete::newline, multi::separated_list0, IResult},
+    parse::integer,
+};
+
+pub(crate) fn numbers(input: &str) -> IResult<&str, Vec<i32>> {
+    separated_list0(newline, integer::i32)(input)
+}
+
+#[test]
+fn test_numbers() {
+    let input = "\
+123
+-456
+";
+    let (_, nums) = numbers(input).unwrap();
+    assert_eq!(nums, vec![123, -456])
+}
