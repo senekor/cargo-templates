686ee36fe9ad9a21f182ebf1051994d10000de7f
Remo Senekowitsch <remo@buenzli.dev>
2023-12-10 23:31:16 +0100

    remove parsing from template
    The aoc_utils crate replaced nom with winnow for parsing.
    It would've been an option to translate the template accordingly.
    But a parsing library is overkill for most puzzles and I'm not
    yet comfortable enough with winnow to make the template idiomatic.
    
    winnow-based parsing may be added back to the template at some point.

--- aoc/{{year}}/{{day}}/src/lib.rs
+++ aoc/{{year}}/{{day}}/src/lib.rs
@@ -1,11 +1,7 @@
-mod parse;
-
 pub fn part1(input: &str) -> usize {
-    let (_, numbers) = parse::numbers(input).unwrap();
     0
 }
 
 pub fn part2(input: &str) -> usize {
-    let (_, numbers) = parse::numbers(input).unwrap();
     0
 }
